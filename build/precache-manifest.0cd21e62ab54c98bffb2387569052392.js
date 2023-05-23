32a4e264b266a97f39fe1f5bec761710e907c2f8
Phuc2003-vietnam <phuc.nguyenpt180728@hcmut.edu.vn>
2023-05-23 22:03:00 +0700

    add

---
+++ build/precache-manifest.0cd21e62ab54c98bffb2387569052392.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af617236750231ce2097fceb2f06090f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9694a6367f11a2f709b1",
+    "url": "/static/css/2.9fc4f277.chunk.css"
+  },
+  {
+    "revision": "c8ccf76cb45aff7b9378",
+    "url": "/static/css/main.7e169c8b.chunk.css"
+  },
+  {
+    "revision": "9694a6367f11a2f709b1",
+    "url": "/static/js/2.fcf1f119.chunk.js"
+  },
+  {
+    "revision": "8c4c6b9f45c977548785dfac0f6ad362",
+    "url": "/static/js/2.fcf1f119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ccf76cb45aff7b9378",
+    "url": "/static/js/main.1b1e1533.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.1b1e1533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ddea268de2a6d49bc17",
+    "url": "/static/js/runtime-main.dae7e130.js"
+  }
+]);
